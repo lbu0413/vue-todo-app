b9ab975eea7a7d02b3500e6ac9e7ad281109a0a0
LBU <noeliden1@gmail.com>
2021-04-29 01:33:40 -0400

    modal created when inputs empty

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -4,14 +4,21 @@
       <span class="addContainer" v-on:click="addTodo">
           <i class="fas fa-plus addBtn"></i>
       </span>
+      <Modal v-if="showModal" @close="showModal = false">
+          <h3 slot="header">:(</h3>
+          <h3 slot="body">can't think of something to do?</h3>
+      </Modal>
   </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue';
+
 export default {
     data: function() {
         return {
-            newTodoItem: ""
+            newTodoItem: "",
+            showModal: false
         }
     },
     methods: {
@@ -19,11 +26,16 @@ export default {
             if(this.newTodoItem !== '') {
                 this.$emit('addTodoItem', this.newTodoItem)
                 this.clearInput();
+            } else {
+                this.showModal = !this.showModal;
             }
         },
         clearInput: function() {
             this.newTodoItem = '';
         }
+    },
+    components: {
+        Modal: Modal,
     }
 }
 </script>
